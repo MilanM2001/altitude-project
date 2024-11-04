83311392ae0fc8ec2ff9b7de64a46b8b647344f9
MilanM2001 <miljusmilan44@gmail.com>
2024-11-04 13:03:44 +0100

    first commit

---
+++ Backend/Models/Enums/Role.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Backend.Models.Enums
+{
+    public enum Role
+    {
+        [EnumMember(Value = "Admin")]
+        Admin,
+
+        [EnumMember(Value = "User")]
+        User,
+    }
+}
---
+++ Backend/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using Backend.Models.Enums;
+
+namespace Backend.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [EmailAddress]
+        [MinLength(5)]
+        [MaxLength(70)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Surname { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public Role Role { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
